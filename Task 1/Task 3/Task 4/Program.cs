315e717672218d257eb371f46d5ecce53a4321b1
Bogdan <hurasy71@gmail.com>
2022-12-26 21:44:43 +1000

    Task 4

---
+++ Task 1/Task 3/Task 4/Program.cs
@@ -0,0 +1,25 @@
+﻿uint oddNumberCount = 0; 
+uint evenNumberCount = 0;
+
+System.Console.WriteLine("Введите первое число диапозона");
+int  currentValue = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите последнее число диапозона");
+int limit = int.Parse(Console.ReadLine());
+
+while (currentValue <= limit)
+{
+    if (currentValue % 2 == 0)
+    {
+        evenNumberCount++;
+    }
+    else
+    {
+        oddNumberCount++;
+    }
+    currentValue++;
+}
+
+System.Console.WriteLine("Количество нечетных чисел");
+System.Console.WriteLine("Количество четных чисел");
+Console.ReadLine();
