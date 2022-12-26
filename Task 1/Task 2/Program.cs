2a74e2091bb671c149abe9783a12899a67957700
Bogdan <hurasy71@gmail.com>
2022-12-26 20:44:33 +1000

    Task 2

---
+++ Task 1/Task 2/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.Write("Введите первое число");     
+  int number1 = Convert.ToInt32(System.Console.ReadLine());
+
+  System.Console.Write("Введите второе число");
+  int number2 = Convert.ToInt32(System.Console.ReadLine());
+
+  System.Console.Write("Введите третье число");     
+  int number3 = Convert.ToInt32(System.Console.ReadLine());
+
+ int number;
+
+ int max = 0;
+
+for ( int i = 0; i < 3; i++)
+{
+    number = Convert.ToInt32(Console.ReadLine());
+  {
+        max = number;
+  }
+}
+
+System.Console.WriteLine(max);
